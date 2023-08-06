5e4fcb7185c96169a2307e1960355309841bd26f
Ihab Awad <ihab.awad@gmail.com>
2023-08-06 16:06:22 -0700

    Modified altitude algorithm.

--- src/airball/model/aerodynamics.cpp
+++ src/airball/model/aerodynamics.cpp
@@ -25,19 +25,8 @@ double q_to_tas(double q, double p, double t) {
   return sqrt(2.0 * q / dry_air_density(p, t));
 }
 
-static constexpr double kAltK = 1.313e-05;
-static constexpr double kAltN = 0.1903;
-static constexpr double kAltPb = 29.92126;
-
-// The formulae are given in English units. To maintain a consistent API
-// throughout our system, the function inputs and outputs are in SI units. The
-// tradeoff is we do a few more unit conversions than needed, in return for a
-// hopefully less error-prone set of APIs.
 double pressure_to_altitude(double t, double p, double qnh) {
-  double ratio = (qnh / p);
-  double t_kelvin = t + 273.15;
-  double power = 1 / 5.257;
-  return (pow(ratio, power) - 1) * t_kelvin / 0.0065;
+  return 44330.0 * (1.0 - pow(p / qnh, 1.0 / 5.255));
 }
 
 } // namespace airball
